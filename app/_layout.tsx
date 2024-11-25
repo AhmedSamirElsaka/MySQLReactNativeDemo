cefb1207dbc45951e3474f9523153779b3e3018f
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-11-25 10:30:56 +0200

    insert demo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,60 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [form, setForm] = useState({ RollNo: "", StudentName: "", Course: "" });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+  const handleSubmit = () => {
+    if (form.RollNo && form.StudentName && form.Course) {
+      var InsertAPIURL = "http://10.0.2.2:80/api/insert.php";
+      var headers = {
+        Accept: "application/json",
+        "Content-Type": "application.json",
+      };
+
+      var data = {
+        RollNo: form.RollNo,
+        StudentName: form.StudentName,
+        Course: form.Course,
+      };
+
+      fetch(InsertAPIURL, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } else {
+      alert("Please fill all the fields");
     }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+  };
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View style={{ flex: 1, padding: 20, gap: 10 }}>
+      <TextInput
+        placeholder="RollNo"
+        value={form.RollNo}
+        onChangeText={(value) => setForm({ ...form, RollNo: value })}
+      />
+      <TextInput
+        placeholder="Student Name"
+        value={form.StudentName}
+        onChangeText={(value) => setForm({ ...form, StudentName: value })}
+      />
+      <TextInput
+        placeholder="Course"
+        value={form.Course}
+        onChangeText={(value) => setForm({ ...form, Course: value })}
+      />
+
+      <Button title="Submit" onPress={handleSubmit} />
+    </View>
   );
 }
