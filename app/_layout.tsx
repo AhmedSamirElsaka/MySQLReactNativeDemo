b4d947d14426093bf94de7054501fbf8684f75d2
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-11-25 12:38:58 +0200

    add the search in database with php

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,14 @@ import { useState } from "react";
 import { Button, Text, TextInput, View } from "react-native";
 
 export default function RootLayout() {
-  const [form, setForm] = useState({ RollNo: "", StudentName: "", Course: "" });
+  const [form, setForm] = useState({
+    RollNo: "",
+    StudentName: "",
+    Course: "",
+    FindRollNo: "",
+  });
 
-  const handleSubmit = () => {
+  const handleInsertSubmit = () => {
     if (form.RollNo && form.StudentName && form.Course) {
       var InsertAPIURL = "http://10.0.2.2:80/api/insert.php";
       var headers = {
@@ -36,8 +41,47 @@ export default function RootLayout() {
       alert("Please fill all the fields");
     }
   };
+
+  const handleSearchSubmit = () => {
+    if (form.FindRollNo) {
+      var InsertAPIURL = "http://10.0.2.2:80/api/search.php";
+      var headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      };
+
+      var data = {
+        FindRollNo: form.FindRollNo,
+      };
+
+      fetch(InsertAPIURL, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setForm({
+            ...form,
+            StudentName: data[0].StudentName,
+            Course: data[0].Course,
+            RollNo: data[0].RollNo,
+          });
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } else {
+      alert("Please fill all the fields");
+    }
+  };
   return (
     <View style={{ flex: 1, padding: 20, gap: 10 }}>
+      <TextInput
+        placeholder="FindRollNo"
+        value={form.FindRollNo}
+        onChangeText={(value) => setForm({ ...form, FindRollNo: value })}
+      />
       <TextInput
         placeholder="RollNo"
         value={form.RollNo}
@@ -54,7 +98,8 @@ export default function RootLayout() {
         onChangeText={(value) => setForm({ ...form, Course: value })}
       />
 
-      <Button title="Submit" onPress={handleSubmit} />
+      <Button title="Submit" onPress={handleInsertSubmit} />
+      <Button title="Search" onPress={handleSearchSubmit} />
     </View>
   );
 }
